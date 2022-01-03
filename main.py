7f8b141841e76fee543a764616ee7fb18d50afcf
Alexandre Devaux <alexandre.devaux@epita.fr>
2022-01-03 23:38:23 +0100

    safe for public

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ import random
 # generate a random number
 
 file = "output.txt"
+directory = "C:\\Users\\kakoumet\\OneDrive - Microsoft Office 365\\Documents\\BotCommits\\"
 
 def commit(file, new_text):
-    
-    # remove content
+    # remove content of file
     f = open(file, "w")
     f.close()
-    os.system(f"cd C:\\Users\\kakoumet\\OneDrive - Microsoft Office 365\\Documents\\BotCommits "
+    os.system(f"cd {directory} "
               f"&& echo {new_text} > output.txt "
               f"&& git add {file} "
               f"&& git commit -m \"commit of {new_text}\" "
               f"&& git push origin master")
 
 
-nb_commits = random.randint(0,25)
-print(f"nb_commits : {nb_commits}")
-
 if __name__ == '__main__':
+    nb_commits = random.randint(0,25)
+    print(f"nb_commits : {nb_commits}")
     for i in range(nb_commits):
         newText = str(random.randint(1,100000))
         nb_number = random.randint(1, 1000)
